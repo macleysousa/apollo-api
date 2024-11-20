6e04111cbebaf7cd1a5f25bac26810ee1cafdf16
Macley Sousa <macleysousa@hotmail.com>
2024-11-20 10:40:38 -0300

    fix: set default database type to 'mysql' and update timezone to '-03:00'

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -13,7 +13,7 @@ if (!DB_TYPE || !DB_HOSTNAME || !DB_DATABASE || !DB_USERNAME) {
 const { ORM_LOGGING, DB_PASSWORD, DB_PORT, DB_TIMEZONE } = process.env;
 
 export default {
-  type: DB_TYPE,
+  type: DB_TYPE ?? 'mysql',
   host: DB_HOSTNAME,
   username: DB_USERNAME,
   password: DB_PASSWORD,
@@ -25,5 +25,5 @@ export default {
   entities: [join(__dirname, DB_ENTITIES ?? '../modules/**/*.entity{.ts,.js}')],
   migrations: [join(__dirname, DB_MIGRATIONS ?? '../migrations/*{.ts,.js}')],
   migrationsRun: process.env.NODE_ENV === 'development' ? false : true,
-  timezone: DB_TIMEZONE || '+00:00',
+  timezone: DB_TIMEZONE || '-03:00',
 } as TypeOrmModuleOptions;
