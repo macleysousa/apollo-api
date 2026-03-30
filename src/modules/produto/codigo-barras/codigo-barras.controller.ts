e85c51b22a1f02d67428a9200f8b81f3cdba2685
fabricioJunior <34898688+fabricioJunior@users.noreply.github.com>
2026-03-30 16:10:33 -0300

    fix: falha ao iniciar a API por controller duplicado.

--- src/modules/produto/codigo-barras/codigo-barras.controller.ts
+++ src/modules/produto/codigo-barras/codigo-barras.controller.ts
@@ -1,26 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { ParseIntPipe } from '@nestjs/common/pipes';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+import { ContextService } from 'src/context/context.service';
 import { ApiComponent } from 'src/decorators/api-componente.decorator';
+import { ApiEmpresaAuth } from 'src/decorators/api-empresa-auth.decorator';
+import { EstoqueView } from 'src/modules/estoque/views/estoque.view';
 
-import { CodigoBarrasService } from './codigo-barras.service';
+import { CodigoBarrasResumo, CodigoBarrasService } from './codigo-barras.service';
 import { CreateCodigoBarrasDto } from './dto/create-codigo-barras.dto';
 
 @ApiTags('Produtos')
-@Controller('produtos/:id/codigo-barras')
+@Controller('produtos')
 @ApiBearerAuth()
 @ApiComponent('PRDFM009', 'Manutenção de codigo de barras')
 export class CodigoBarrasController {
-  constructor(private readonly service: CodigoBarrasService) {}
+  constructor(
+    private readonly service: CodigoBarrasService,
+    private readonly contextService: ContextService,
+  ) {}
 
-  @Post()
+  @Post(':id/codigo-barras')
   async create(@Param('id', ParseIntPipe) id: number, @Body() createDto: CreateCodigoBarrasDto): Promise<void> {
     await this.service.create(id, createDto);
   }
 
-  @Delete(':barcode')
+  @Delete(':id/codigo-barras/:barcode')
   async remove(@Param('id', ParseIntPipe) id: number, @Param('barcode') barcode: string): Promise<void> {
     await this.service.remove(id, barcode);
+  }
+
+  @Get('codigo-barras')
+  @ApiQuery({ name: 'tipo', required: false, enum: ['EAN13', 'RFID'] })
+  async findCodigos(@Query('tipo') tipo?: 'EAN13' | 'RFID'): Promise<CodigoBarrasResumo[]> {
+    return this.service.findCodigos(tipo);
+  }
+
+  @Get('codigo-barras/:codigo/produto')
+  @ApiEmpresaAuth()
+  async findProdutoByCodigo(@Param('codigo') codigo: string): Promise<EstoqueView> {
+    return this.service.findProdutoByCodigo(codigo, this.contextService.empresaId());
   }
 }
--- src/modules/produto/codigo-barras/codigo-barras.module.ts
+++ src/modules/produto/codigo-barras/codigo-barras.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstoqueView } from 'src/modules/estoque/views/estoque.view';
 
-import { CodigoBarrasQueryController } from './codigo-barras-query.controller';
 import { CodigoBarrasController } from './codigo-barras.controller';
 import { CodigoBarrasService } from './codigo-barras.service';
 import { CodigoBarrasEntity } from './entities/codigo-barras.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CodigoBarrasEntity, EstoqueView])],
-  controllers: [CodigoBarrasController, CodigoBarrasQueryController],
+  controllers: [CodigoBarrasController],
   providers: [CodigoBarrasService],
   exports: [CodigoBarrasService],
 })
